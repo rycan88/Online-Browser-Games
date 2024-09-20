7206571ab76de977c9051ecf7751995456331a01
rycan88 <rycan888@gmail.com>
2024-09-20 00:55:29 -0700

    Remove deleted files

--- src/pages/ErrorPage.js
+++
@@ -1,8 +0,0 @@
-export const ErrorPage = () => {
-    return (
-        <div className="entirePage justify-center items-center">
-            <h1 className="text-6xl">PAGE DOES NOT EXIST</h1>
-        </div>
-    );
-
-}

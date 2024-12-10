5e406c0565826051ccbf884813f239d7d008cee6
rycan88 <rycan888@gmail.com>
2024-12-10 00:59:51 -0800

    Added rps melee to the games page

--- frontend/src/components/GamesData.js
+++ frontend/src/components/GamesData.js
@@ -1,20 +1,30 @@
 export const GamesData = [
     {
+        id: "odd_colour_out",
         title: "Odd Colour Out",
         logoLink: "/odd-colour-out-logo.png",
         link: "/odd_colour_out",
         description: "Can you spot the odd colour out? Choose the differing colours until they blend in too much to see the difference!"
     },
     {
+        id: "telepath",
         title: "Telepath",
         logoLink: "/telepath-logo.png",
         link: "/telepath/lobby",
         description: "Are you on the same wavelength as your friends? This fun word game will test your compatibility with your friends!"
     },
     {
+        id: "thirty_one",
         title: "31",
         logoLink: "/thirty-one-logo.png",
         link: "/thirty_one/lobby",
         description: "Collect cards of the same suit to reach 31!"
+    },
+    {
+        id: "rock_paper_scissors_melee",
+        title: "RPS Melee",
+        logoLink: "/rps-melee-logo.png",
+        link: "/rock_paper_scissors_melee/lobby",
+        description: "Predict your opponents moves in this fast paced rock paper scissors game!"
     },
 ]

e6d8de7a5dafb191c7bca31a5a8063b064f63f5d
Ryan Leeson <rycan888@gmail.com>
2026-03-16 01:18:18 -0700

    Added lines in settings

--- frontend/src/components/SectionHeading.js
+++ frontend/src/components/SectionHeading.js
@@ -1,12 +1,16 @@
 
-export const SectionHeading = ({text, color="slate-300"}) => {
+export const SectionHeading = ({text, color="rgb(203,213,225)"}) => { // slate-300
     return (
-        <div className={`flex items-center gap-3 text-${color} text-sm font-semibold uppercase tracking-wider`}
+        <div className={`flex items-center gap-3 text-sm font-semibold uppercase tracking-wider`}
              style={{color: color}}
         >
-            <div className={`flex-1 h-[2px] bg-${color}/50`}/>
+            <div className={`flex-1 h-[2px]`}
+                 style={{backgroundColor: color, opacity: 0.5}}
+            />
             {text}
-            <div className={`flex-1 h-[2px] bg-${color}/50`}/>
+            <div className={`flex-1 h-[2px]`}
+                 style={{backgroundColor: color, opacity: 0.5}}
+            />
         </div>
     );
 }
